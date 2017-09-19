e276375a225dd3f7dcf709918f963e4502c6234e
Jeff Palin <jeffpalin@gmail.com>
2017-09-19 13:17:16 -0700

    initial commit

---
+++ bamazonDB.sql
@@ -0,0 +1,46 @@
+DROP DATABASE IF EXISTS bamazonDB;
+
+CREATE DATABASE bamazonDB;
+
+USE bamazonDB;
+
+CREATE TABLE products (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NULL,
+  department_name VARCHAR(255) NULL,
+  price INTEGER(10) NULL,
+  stock_quantity INTEGER(10) NULL,
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("IPhone X", "Electronics", "999", "25");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("TV", "electronics", "650", "50");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("bicycle", "outdoor", "850", "12");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tent", "outdoor", "220", "18");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("jeans", "clothing", "120", "32");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("shirt", "clothing", "60", "85");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("couch", "home goods", "650", "20");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("coffee maker", "home goods", "75", "30");
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES ("tire", "automotive", "130", "40");
+
+
+SELECT*FROM products;
+
+
